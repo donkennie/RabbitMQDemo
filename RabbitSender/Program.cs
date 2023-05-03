514ab40e8c59afb9fd0c98fd1edb73d3c6597b19
donkennie <ajeigbekehinde160@gmail.com>
2023-05-03 05:15:19 +0100

    added two receivers project to served as server for any request

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.Text;
 
 ConnectionFactory factory = new();
 factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
@@ -11,3 +12,18 @@ IModel channel = cnn.CreateModel();
 string exchangeName = "DemoExchange";
 string routingKey = "demo-routing-key";
 string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending Message {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message{i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+cnn.Close();
